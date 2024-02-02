29e71dc9959dba2638726efd7a9d7941ac5f342a
DDangDin <kcorelec7123@gmail.com>
2024-02-02 16:00:52 +0900

    initial commit

---
+++ app/src/main/java/com/team/flip/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.team.flip
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.team.flip.ui.theme.FlipTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlipTheme {
+                // Initial Project (23.01.03)
+            }
+        }
+    }
+}
