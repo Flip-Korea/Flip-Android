cfcd8746a801894f95481f9ad91aa380a46c3349
DDangDin <Kcorelec7123@gmail.com>
2025-01-05 00:29:33 +0900

    FS-9 [chore] errorBodyFirst 수정
    - errorBodyFirst 수정에 따른 파일 수정

--- presentation/src/main/java/com/team/presentation/util/uitext/errorBodyFirst.kt
+++ presentation/src/main/java/com/team/presentation/util/uitext/errorBodyFirst.kt
@@ -2,6 +2,7 @@ package com.team.presentation.util.uitext
 
 import com.team.domain.util.ErrorBody
 import com.team.domain.util.ErrorType
+import com.team.domain.util.Result
 
 /**
  * 에러바디(message)를 먼저 내보내고 만약 Null이라면 사전 정의된 ErrorType를 내보낸다.
@@ -9,11 +10,7 @@ import com.team.domain.util.ErrorType
  * [ErrorBody.errors]는 제외 된 상태
  */
 // TODO: 더 다양한 처리가 가능하게끔 리팩토링 필요
-fun errorBodyFirst(
-    errorBody: ErrorBody?,
-    error: ErrorType,
-): UiText {
-    return errorBody?.let { errBody ->
+fun <T> Result.Error<T, ErrorType>.errorBodyFirst(): UiText =
+    errorBody?.let { errBody ->
         UiText.DynamicString(errBody.message)
     } ?: error.asUiText()
-}
